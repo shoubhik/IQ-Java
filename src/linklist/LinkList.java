c33473254fbc41e5f9e1c6cd5e0b794549415c7b
shoubhik <shoubhikbhatti@gmail.com>
2013-01-25 18:28:34 -0500

    new questions

--- src/linklist/LinkList.java
+++ src/linklist/LinkList.java
@@ -9,13 +9,26 @@ public class LinkList {
 
         public int data;
         public LinkListNode next;
+        public LinkListNode random;
+
 
         public LinkListNode(int data){
             this.data = data;
             this.next = null;
+            this.random = null;
         }
     }
+
+    public static LinkListNode getNewNode(int data){
+        return new LinkListNode(data);
+    }
     private LinkListNode head;
+
+    private boolean hasRandomNode = false;
+
+    public LinkList(){
+        this.head = null;
+    }
 
     public LinkList(int data){
         this.head = new LinkListNode(data);
@@ -25,8 +38,62 @@ public class LinkList {
         addLast(arr);
     }
 
+    private LinkList(boolean hasRandomNode){
+        this.hasRandomNode = hasRandomNode;
+        this.head = null;
+    }
+
+    public static LinkList getListWithRandomLinks(int arr[], int randomLinks[]){
+        LinkList linkList = new LinkList(true);
+        linkList.addLast(arr);
+        linkList.connectRandomNodes(randomLinks);
+
+        return linkList;
+    }
+
+
+    private void connectRandomNodes(int randomLinks[]) {
+        LinkListNode temp = getHead();
+        for(int idx : randomLinks){
+            temp.random = idx == -1 ? null : getNodeWithIndex(idx);
+            temp = temp.next;
+        }
+    }
+
+    private LinkListNode getNodeWithIndex(int index){
+        LinkListNode temp = getHead();
+        int count = 0;
+        while(temp != null){
+            if(count == index)
+                return temp;
+            else  {
+                count++;
+                temp = temp.next;
+            }
+        }
+        return null;
+    }
+
+    private int getIndexOfNode(LinkListNode node){
+        if(node == null)
+            return -1;
+        int count = 0;
+        LinkListNode temp = getHead();
+        while(temp != node) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+
+
     public LinkListNode getHead() {
         return head;
+    }
+
+    public void setHead(LinkListNode node){
+        this.head = node;
     }
 
     public void addLast(int data){
@@ -56,6 +123,16 @@ public class LinkList {
     }
 
     public void print(){
+
+        System.out.print(toString());
+    }
+
+    public String toString(){
+        return !hasRandomNode ? toStringForSimpleLinkList() :
+                toStringForLinListWithRandomNodes();
+    }
+
+    public String toStringForSimpleLinkList(){
         LinkListNode temp = getHead();
         StringBuilder sb = new StringBuilder();
         sb.append("[ ");
@@ -66,6 +143,29 @@ public class LinkList {
             temp=temp.next;
         }
         sb.append(" ]");
-        System.out.print(sb.toString());
+        return sb.toString();
+    }
+
+
+    public String toStringForLinListWithRandomNodes(){
+        LinkListNode temp = getHead();
+        StringBuilder sb = new StringBuilder();
+        while(temp != null) {
+            sb.append("[ next:(");
+            sb.append(getIndexOfNode(temp));
+            sb.append(",");
+            sb.append(getIndexOfNode(temp.next));
+            sb.append("), random:(");
+            sb.append(getIndexOfNode(temp));
+            sb.append(",");
+            sb.append(getIndexOfNode(temp.random));
+            sb.append(") ]\n");
+            temp = temp.next;
+        }
+        return sb.toString();
+    }
+
+    public void setHasRandomNode(boolean  hasRandomNode){
+        this.hasRandomNode = hasRandomNode;
     }
 }
