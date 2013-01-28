19edecde3414779ec114f308ff6121bbb0ff50c9
shoubhik <shoubhikbhatti@gmail.com>
2013-01-28 00:53:31 -0500

    new questions

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -7,8 +7,12 @@ public class Utils {
     private Utils(){}
 
     public static String printArraySingleLine(int arr[]){
+        return printArraySingleLine(arr, arr.length);
+    }
+
+    public static String printArraySingleLine(int arr[], int idx){
         StringBuilder sb = new StringBuilder();
-        for(int i =0;i < arr.length;i++) sb.append(arr[i] + " ");
+        for(int i =0;i < idx;i++) sb.append(arr[i] + " ");
         return sb.toString();
     }
 
